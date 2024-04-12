d05a21c587b393132a7a5e7ea49e91b367201735
shyboiiSeung <163094594+shyboiiSeung@users.noreply.github.com>
2024-04-12 22:19:32 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    if (N == 1) {
+        printf("Leading the Way to the Future");
+    }
+    else {
+        printf("YONSEI");
+    }
+    return 0;
+}
