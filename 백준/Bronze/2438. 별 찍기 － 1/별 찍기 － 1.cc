4de13574c57fc78e385902ebc28d32e4071ea40d
shyboiiSeung <163094594+shyboiiSeung@users.noreply.github.com>
2024-05-27 16:52:25 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-int main() {
-	int a;
+int main(void) {
+	int a = 0;
 	scanf("%d", &a);
 	for (int i = 1; i <= a; i++) {
-		for (int j = 1; j <= i; j++) {
+		for (int j = 1; j < i; j++) {
 			printf("*");
 		}
-		printf("\n");
+		printf("*\n");
 	}
 	return 0;
 }
